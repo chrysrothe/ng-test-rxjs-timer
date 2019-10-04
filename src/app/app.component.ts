8908d5412d7a28e4bb97fc1fd456bf10e4a7c879
chrysrothe <rothechristian92@gmail.com>
2019-10-04 20:04:45 +0200

    timer service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TimerService } from './services/timer.service';
+import { Status } from './enums/status.enum';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'timer';
+  public status: Status;
+  private timer$: Subscription;
+
+  constructor(private timerService: TimerService){}
+
+  public ngOnInit(): void {
+    this.timer$ = this.timerService.getStaus().subscribe((status: Status) => {
+      this.status = status;
+
+      if(status === Status.InValid) {
+        this.timer$.unsubscribe();
+      }
+    });
+  }
 }
---
+++ src/app/services/timer.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable, timer, of } from 'rxjs';
+
+import { switchMap } from 'rxjs/operators';
+import { Status } from '../enums/status.enum';
+
+const SWITCH_STATUS = 5;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimerService {
+
+  public getStaus(): Observable<Status> {
+    return timer(1000, 1000).pipe(
+      switchMap((time: number) => {
+        if (time < SWITCH_STATUS) {
+          return of(Status.Valid);
+        }
+        return of(Status.InValid);
+      })
+    );
+  }
+}
